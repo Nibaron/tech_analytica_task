09c2d54d6919e8ef7d1250369fe56c89ac7b0560
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-05-17 04:54:56 +0600

    header fix

--- conponents/layout/header.tsx
+++ conponents/layout/header.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+'use client';
+
+import React, { useState } from "react";
 import logo from "@/assets/images/logo.png";
 import Image from "next/image";
+import { DashIcon } from "@/assets/icons";
 
 const Header: React.FC = () => {
+  const [activeLink, setActiveLink] = useState<string >("/products");
+
+  const handleLinkClick = (link: string, event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    setActiveLink(link);
+  };
+
   return (
-    <nav className="bg-slate-900 fixed w-full z-20 top-0 start-0 border-b">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-[12px]">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image src={logo} alt="Logo" />
-        </a>
+    <nav className="bg-slate-900 fixed w-full z-20 top-0 start-0 ">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-[12px]">
+        <Image src={logo} alt="Logo" width={150} />
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
@@ -17,21 +25,27 @@ const Header: React.FC = () => {
             <li>
               <a
                 href="/products"
-                className="block py-2 px-3 text-white relative"
+                className={`block py-2 px-3 text-white relative ${
+                  activeLink === "/products" ? "font-bold" : ""
+                }`}
                 aria-current="page"
+                onClick={(e) => handleLinkClick("/products", e)}
               >
                 PRODUCTS
-                <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-white"></span>
+                {activeLink === "/products" && <DashIcon />}
               </a>
             </li>
             <li>
               <a
                 href="/cart"
-                className="block py-2 px-3 text-white relative"
+                className={`block py-2 px-3 text-white relative ${
+                  activeLink === "/cart" ? "font-bold" : ""
+                }`}
                 aria-current="page"
+                onClick={(e) => handleLinkClick("/cart", e)}
               >
                 CART
-                <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-black"></span>
+                {activeLink === "/cart" && <DashIcon />}
               </a>
             </li>
           </ul>
@@ -39,14 +53,14 @@ const Header: React.FC = () => {
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="m-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             LOGIN
           </button>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center m-4 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
             aria-expanded="false"
           >
